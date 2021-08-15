ff75b4634024bfcfe36d0373a94115ce52a52f55
Mehin <mehin.huseynova97@gmail.com>
2021-08-15 12:03:31 +0100

    wip

--- src/views/login/login.style.ts
+++ src/views/login/login.style.ts
@@ -1,22 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { url } from 'inspector';
 
 export const useStyles = makeStyles((theme) => {
-    const Background = 'https://images.unsplash.com/photo-1457276587196-a9d53d84c58b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9va3NoZWxmJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
     return {
         root: {
             height: '100vh',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            backgroundImage: `url(${Background})`,
-            backgroundPosition: 'center center'
+          
         },
         container: {
             minWidth: 400,
-            background: 'white',
-            padding: '30px',
-            borderRadius: '8px'
         },
         form: {
             display: 'flex',
